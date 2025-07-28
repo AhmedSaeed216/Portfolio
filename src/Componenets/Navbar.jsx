be511c1b991c076a63c652c4a5c6ed98509cda48
Ahmed Saeed <160056886+AhmedSaeed216@users.noreply.github.com>
2025-07-28 13:40:59 +0500

    Update Navbar.jsx

--- src/Componenets/Navbar.jsx
+++ src/Componenets/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
 
                     <div>
 
-                        <a href='/AhmedCV.pdf' download="AhmedCV.pdf">
+                        <a href='/AhmedCV.pdf' download="AhmedCVNew">
                             <button className='bg-white text-black font-xl font-bold rounded-lg w-28 h-11  hover:bg-black hover:text-white border-white '>
                                 Download CV
                                {/* <AnimatedSubscribeButtonDemo/>  */}
