f71a2f69659f85611e2ae864d806fb84526a403f
Ahmed Saeed <160056886+AhmedSaeed216@users.noreply.github.com>
2025-08-12 10:13:55 +0500

    Update Drawer.jsx

--- src/Componenets/Drawer.jsx
+++ src/Componenets/Drawer.jsx
@@ -10,9 +10,9 @@ const Drawer = ({ isopen, toggleDrawer }) => {
                         <li className='text-black hover:underline active:underline cursor-pointer'><a href="#about">About</a></li>
                         <li className='text-black hover:underline active:underline cursor-pointer'><a href="#skills">Skills</a></li>
                         <li className='text-black hover:underline active:underline cursor-pointer'><a href="#contact">Contact me</a></li>
-                        <li className='text-black hover:underline active:underline cursor-pointer'><a href="#project">Projecrs</a></li>
+                        <li className='text-black hover:underline active:underline cursor-pointer'><a href="#project">Projects</a></li>
                         <li>
-                            <a href='/AhmedCV.pdf' download="AhmedCv">
+                            <a href='/AhmedCVNew.pdf' download="AhmedCV">
                                 <button className='text-black hover:underline active:underline cursor-pointer ' >Download CV</button>
                             </a>
                         </li>
